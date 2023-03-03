3198f7b84780057bf45ec24c1e9c66ddd07e473a
AHDesigns <anthony.hancock85@gmail.com>
2023-03-03 12:37:18 +0000

    LoopSW5 initial commit

--- src/WhileExample.java
+++ src/WhileExample.java
@@ -1,10 +1,10 @@
-public class WhileLoop {
+public class WhileExample {
     public static void main(String[] args) {
-        int x = 0;
-        while( x < 20 )
+        int x = 20;
+        while( x >= 10 )
         {
             System.out.print("value of x : " + x );
-            x++;
+            x--;
             System.out.print("\n");
         }
     }
