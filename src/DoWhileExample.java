400af437bad877c8ff684f038c1cf2da036a8dfc
AHDesigns <anthony.hancock85@gmail.com>
2023-03-03 12:54:55 +0000

    added DoWhileLoop

---
+++ src/DoWhileExample.java
@@ -0,0 +1,8 @@
+public class DoWhileExample {
+    public static void main(String[] args) {
+        int x = 1;
+        do {
+            System.out.println("I am executed, yay!");
+        } while (x < 1);
+    }
+}
